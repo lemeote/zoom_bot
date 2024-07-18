4ba1e6ce43001226f7aa7855312d05fc607732cd
lemeote <dorastiku@gmail.com>
2024-07-18 04:20:51 -0400

    update: deploy-project-to-vercel

---
+++ api/oauth.js
@@ -0,0 +1,7 @@
+const clientId = process.env.CLIENT_ID;
+const redirectUri = process.env.REDIRECT_URI;
+
+module.exports = (req, res) => {
+  const authUrl = `https://zoom.us/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+  res.redirect(authUrl);
+};
